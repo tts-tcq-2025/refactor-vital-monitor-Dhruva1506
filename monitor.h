216178cf69fcaa275bc3a5473af510c94804cb49
Dhruva1506 <vidushikumari15@gmail.com>
2025-08-04 11:36:17 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,3 +1,21 @@
 #pragma once
 
-int vitalsOk(float temperature, float pulseRate, float spo2);
+#ifndef MONITOR_H
+#define MONITOR_H
+
+enum class VitalStatus {
+    OK,
+    TemperatureOutOfRange,
+    PulseOutOfRange,
+    Spo2OutOfRange
+};
+
+// Pure function: check vitals, return status
+VitalStatus checkVitals(float temperature, float pulseRate, float spo2);
+
+// Alert function: handle alerts with blinking and messages
+void alertIfNeeded(VitalStatus status);
+
+#endif // MONITOR_H
+
+
