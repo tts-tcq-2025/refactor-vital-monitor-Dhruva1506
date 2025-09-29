898aab4f2d16b6f5119793adef88b08c1dc69491
Dhruva1506 <vidushikumari15@gmail.com>
2025-09-29 17:18:27 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,8 +1,24 @@
 #pragma once
+#include <string>
+#include <vector>
+
+enum class VitalStatus { NORMAL, LOW, HIGH };
+
+struct VitalCheck {
+  std::string name;
+  float value;
+  float min;
+  float max;
+};
+
 
 bool isTemperatureCritical(float temperature);
 bool isPulseRateOutOfRange(float pulseRate);
 bool isSpo2Low(float spo2);
 
-int vitalsOk(float temperature, float pulseRate, float spo2);
+VitalStatus evaluateVital(float value, float min, float max);
+std::vector<std::string> checkVitals(const std::vector<VitalCheck>& vitals);
 
+
+void blinkWarningMessage(const char* message);
+bool vitalsOk(float temperature, float pulseRate, float spo2);
