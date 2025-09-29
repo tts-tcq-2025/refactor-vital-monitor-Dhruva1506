3bca755486606f45079097be55200f6857e20a6d
Dhruva1506 <vidushikumari15@gmail.com>
2025-09-29 17:25:32 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -6,18 +6,6 @@
 
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
-bool isTemperatureCritical(float temperature) {
-  return temperature > 102 || temperature < 95;
-}
-
-bool isPulseRateOutOfRange(float pulseRate) {
-  return pulseRate < 60 || pulseRate > 100;
-}
-
-bool isSpo2Low(float spo2) {
-  return spo2 < 90;
-}
-
 VitalStatus evaluateVital(float value, float min, float max) {
   if (value < min) return VitalStatus::LOW;
   if (value > max) return VitalStatus::HIGH;
@@ -28,22 +16,26 @@ std::vector<std::string> checkVitals(const std::vector<VitalCheck>& vitals) {
   std::vector<std::string> warnings;
   for (const auto& vital : vitals) {
     VitalStatus status = evaluateVital(vital.value, vital.min, vital.max);
-    if (status == VitalStatus::LOW || status == VitalStatus::HIGH) {
+    if (status != VitalStatus::NORMAL) {
       warnings.push_back(vital.name + " out of range!");
     }
   }
   return warnings;
 }
 
-void blinkWarningMessage(const char* message) {
+void blinkWarningMessage(const std::string& message) {
   cout << message << '\n';
-  for (int i = 0; i < 6; i++) {
-    cout << "\r* " << flush;
-    sleep_for(seconds(1));
-    cout << "\r *" << flush;
+
+#ifdef ENABLE_BLINKING
+  for (int i = 0; i < 6; ++i) {
+    cout << (i % 2 == 0 ? "\r* " : "\r *") << flush;
     sleep_for(seconds(1));
   }
   cout << "\r  \r" << flush;  // Clear the line
+#else
+ 
+  cout << "[BLINKING DISABLED in test mode]\n";
+#endif
 }
 
 bool vitalsOk(float temperature, float pulseRate, float spo2) {
@@ -54,7 +46,7 @@ bool vitalsOk(float temperature, float pulseRate, float spo2) {
 
   auto warnings = checkVitals(vitals);
   if (!warnings.empty()) {
-    blinkWarningMessage(warnings.front().c_str());  // Show only first issue
+    blinkWarningMessage(warnings.front());  
     return false;
   }
   return true;
